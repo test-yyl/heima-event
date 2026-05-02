8d9b9e68cef7373a50ba68375da5679933f29800
test-yyl <2700235686@qq.com>
2026-05-02 10:50:06 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,26 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'node:path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
-  ,
-  server:{
-    proxy:{
-      '/api':{//获取路径中包含了/api的请求
-          target:'http://localhost:6666',//后台服务所在的源
-          changeOrigin:true,//修改源
-          rewrite:(path)=>path.replace(/^\/api/,'')///api替换为''
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:6666',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
+  },
+  // 添加构建配置
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
   }
 })
